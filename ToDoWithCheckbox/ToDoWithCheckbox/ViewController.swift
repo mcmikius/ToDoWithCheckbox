017b4299532f2dbdb6322744650314842619cc47
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-01-11 20:11:15 +0200

    init

---
+++ ToDoWithCheckbox/ToDoWithCheckbox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoWithCheckbox
+//
+//  Created by Mykhailo Bondarenko on 11.01.2020.
+//  Copyright © 2020 Mykhailo Bondarenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
