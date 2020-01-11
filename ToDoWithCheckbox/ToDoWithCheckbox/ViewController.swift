916f1298b30cd73c2f3c126b8913d8c66dda366d
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-01-11 20:20:48 +0200

    add delegate methods

--- ToDoWithCheckbox/ToDoWithCheckbox/ViewController.swift
+++ ToDoWithCheckbox/ToDoWithCheckbox/ViewController.swift
@@ -9,11 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 
 
 }
